0b3457c8e9a7c08720bebd642f20f789bcafdc35
Abhishek Arya <aarya@acuantcorp.com>
2018-05-29 15:01:33 -0700

    Clean the project

--- IDHTML5ampleApp/js/AcuantWebServiceJS/credentials.js
+++ IDHTML5ampleApp/js/AcuantWebServiceJS/credentials.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     //Credentials to access the AssureID Connect Web Service.
  username = ""; // use your AssureID Connect username & password
 password = ""; // use your AssureID Connect username & password
-assureIDConnectEndpoint = ""; // set the AssureID Connect REST Endpoint URL address
+assureIDConnectEndpoint = "https://services.assureid.net/"; // set the AssureID Connect REST Endpoint URL address
 subscriptionID = "";
 	
 });
