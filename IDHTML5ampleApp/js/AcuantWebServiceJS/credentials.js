dc739f178d5735c64388e19f4140f8d1f11fdc92
Abhishek Arya <aarya@acuantcorp.com>
2018-06-07 15:50:45 -0700

    Fix a bug with the directory path.

--- IDHTML5ampleApp/js/AcuantWebServiceJS/credentials.js
+++ IDHTML5ampleApp/js/AcuantWebServiceJS/credentials.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function () {
     //Credentials to access the AssureID Connect Web Service.
- username = ""; // use your AssureID Connect username & password
-password = ""; // use your AssureID Connect username & password
-assureIDConnectEndpoint = "https://services.assureid.net/"; // set the AssureID Connect REST Endpoint URL address
-subscriptionID = "";
-	
+    username = ""; // use your AssureID Connect username & password
+    password = ""; // use your AssureID Connect username & password
+    assureIDConnectEndpoint = "https://services.assureid.net/"; // set the AssureID Connect REST Endpoint URL address
+    subscriptionID = "";
+
 });
